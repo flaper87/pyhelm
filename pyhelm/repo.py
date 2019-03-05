69b508dcd78dd37103a0dd36dee43a15e9e14e6a
Rob King <rob.king@celect.com>
2019-03-05 11:07:19 -0500

    Ignore non-numeric/dot characters in semver sort

--- pyhelm/repo.py
+++ pyhelm/repo.py
@@ -46,7 +46,7 @@ class RepositoryError(RuntimeError):
 
 
 def _semver_sorter(x):
-    return list(map(int, x['version'].split('.')))
+    return list(map(int, filter(lambda k: k in '0123456789.', x['version']).split('.')))
 
 def _get_from_http(repo_url, file_url, **kwargs):
     """Downloads the Chart's repo index from HTTP(S)"""
