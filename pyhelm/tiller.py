5c1b202f8c9137c44bbb7a22af1a0f1ea1da3860
Bart Swedrowski <bart@timedout.org>
2019-08-06 12:39:20 +0100

    Update reference to ReleaseServiceStub class
    As per notice in
    https://github.com/flaper87/pyhelm/blob/0.1.5/hapi/services/tiller_pb2.py#L1276-L1277
    ReleaseServiceStub has been moved to *_pb2_grpc.py file

--- pyhelm/tiller.py
+++ pyhelm/tiller.py
@@ -2,9 +2,10 @@ import grpc
 import yaml
 import pyhelm.logger as logger
 
-from hapi.services.tiller_pb2 import ReleaseServiceStub, ListReleasesRequest, \
+from hapi.services.tiller_pb2 import ListReleasesRequest, \
     InstallReleaseRequest, UpdateReleaseRequest, UninstallReleaseRequest, \
     GetReleaseStatusRequest, GetReleaseContentRequest
+from hapi.services.tiller_pb2_grpc import ReleaseServiceStub
 from hapi.chart.chart_pb2 import Chart
 from hapi.chart.config_pb2 import Config
 from hapi.release.status_pb2 import _STATUS
